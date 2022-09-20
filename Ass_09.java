f1dd9a346a05cbef6ddfab3411133a92f9b0108d
neelamgaidhani <113960354+neelamgaidhani@users.noreply.github.com>
2022-09-20 11:30:24 +0530

    demo
    myrepo

---
+++ Ass_09.java
@@ -0,0 +1,23 @@
+package Assignment;
+
+public class Ass_09 {
+	/* Assignment No. 9: Program-1
+	* WAP in which create a multiple objects and
+	* display the count of the objects into class.
+	*/ 
+	static int countobjects;
+	Ass_09() {
+		countobjects++;}
+	public static void getcountobject(){
+		System.out.println(countobjects);}
+	public static void main(String[] args) {
+		Ass_09 a=new Ass_09();
+		Ass_09 b=new Ass_09();
+		Ass_09 c=new Ass_09();
+		Ass_09 d=new Ass_09();
+		Ass_09 e=new Ass_09();
+		System.out.println();
+		getcountobject();
+	}
+
+}
